41896b3ace19bca51eaea5ec7eaff3f525d7ebd8
Partha Sarathi Sarkar <partha.sarathi.sarkar95@outlook.com>
2025-02-21 01:34:07 +0530

    added static

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // ... other configurations ...
+    organizationName: 'partha-sarathi-sarkar',
+    projectName: 'bicepwhiz',
+    url: 'https://partha-sarathi-sarkar.github.io',
+    baseUrl: '/bicepwhiz/',
+    // ... other configurations ...
+  };
+  
